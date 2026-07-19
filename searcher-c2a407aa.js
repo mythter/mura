4e4a08b63475a06ced5eabfdde5d3ca8eb102820
mythter <mythter@users.noreply.github.com>
2026-07-19 13:48:07 +0000

    Deploying to gh-pages from @ mythter/mura@50ab261686622dc098e68b519d55f0c0d7acfd34 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d6a3067b.js',
+                path_to_root + 'searchindex-d9de2514.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
